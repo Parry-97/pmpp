3d68179c3ffc2c195b3db2b92ea445312672ec20
Param Pal Singh <singhparampal97@gmail.com>
2025-10-20 21:04:03 +0200

    organize code

--- src/cuda/vector_add/main.cu
+++ src/cuda/vector_add/main.cu
@@ -2,6 +2,8 @@
 
 __global__ void hello_from_gpu() { printf("Hello from the GPU!\n"); }
 
+__global__ void vecAdd() { printf("Hello from the GPU!\n"); }
+
 int main() {
   printf("Hello from the CPU!\n");
   hello_from_gpu<<<1, 1>>>();
